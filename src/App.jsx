4824de4b4e231953255c0f2972a5cf8e78b10e85
dwvux0812 <khanhdu1211@gmail.com>
2023-07-31 23:25:27 +0700

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Settings from "./pages/Settings";
+import NewUsers from "./pages/Users";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+
+
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to={"dashboard"} />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<NewUsers />} />
+          <Route path="login" element={<Login />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Bookings />
+    </div>
+  );
+};
+
+export default AppLayout;
