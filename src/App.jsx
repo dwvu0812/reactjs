edb9dc3ce26d5a5f1aaf585c160ab92e743f0dab
dwvux0812 <khanhdu1211@gmail.com>
2023-08-06 00:16:51 +0700

    complete toaster

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,66 @@ import Login from "./pages/Login";
 import Account from "./pages/Account";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    }
+  }
+})
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to={"dashboard"} />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<NewUsers />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to={"dashboard"} />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
-          <Route path="account" element={<Account />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ccuirahmqevbhdxhqcdg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdWlyYWhtcWV2YmhkeGhxY2RnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTA4Mjg1OCwiZXhwIjoyMDA2NjU4ODU4fQ.v2sRH8KwPlL1BLCQnpgUXfuvmliGsCUuLlRoVyyVVnY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
